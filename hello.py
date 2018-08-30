43044eed67c0375eba53ae01eb5c942948bd9b30
chenyi5230 <771205821@qq.com>
2018-08-31 00:24:40 +0800

    第四次提交

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+
+
+
+print("Hello everyone.")
+
+print("Oh no, I breke the peoject!")
+print("I breke the peoject!")
+
+print("Hello Python World! how are you?")
+
+print("Hello Python World! how are you1111111?")
+
+
+print("dddddaaaaaa")
